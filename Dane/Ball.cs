6604fa74b113720915b407ee18214cdc3494b3f5
konradkoza <kozakonrad72@gmail.com>
2023-05-11 17:55:53 +0200

    IBall

---
+++ Dane/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class Ball : IBall
+    {
+
+        public Ball(float x, float y, int mass, Vector2 velocity ) {
+            _position = new Vector2(x, y);
+            _velocity = velocity;
+            Mass = mass;
+        
+        }
+
+        public event EventHandler<BallChangedEventArgs>? BallChanged;
+
+        private Vector2 _position;
+
+        public Vector2 Position => _position;
+
+        private Vector2 _velocity;
+
+        public Vector2 Velocity
+        {
+            get => _velocity;
+            set => _velocity = value;
+        }
+
+        public float X => _position.X;
+
+        public float Y => _position.Y;
+
+        public int Mass { get; }
+
+
+        public int Id { get; }
+
+        
+    }
+}
---
+++ Dane/IBall.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal interface IBall
+    {
+        int  Id { get; }
+
+        Vector2 Position { get; }
+        Vector2 Velocity { get; set; }
+
+        int Mass { get; }
+
+        event EventHandler<BallChangedEventArgs> BallChanged;
+
+        float X { get; }
+        float Y { get; }
+
+    
+    }
+}
